b01c45ffc99b88042510e4f651d6dc3a7e44cb8a
Joel Richard Benoit <joel.r.benoit@gmail.com>
2016-01-11 17:21:27 -0800

    Rest Services JSON Jquery
    Accessing and CRUD database

---
+++ WebApplication6/WebApplication6/Controllers/ProductsController.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using System.Web.Http.Description;
+using WebApplication6;
+
+namespace WebApplication6.Controllers
+{
+    public class ProductsController : ApiController
+    {
+        private JB_FoodStoreEntities db = new JB_FoodStoreEntities();
+
+        // GET: api/Products
+        public IQueryable<Product> GetProducts()
+        {
+            JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+            context.Configuration.LazyLoadingEnabled = false;
+            return context.Products;
+        }
+
+        // GET: api/Products/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult GetProduct(int id)
+        {
+            JB_FoodStoreEntities context = new JB_FoodStoreEntities();
+            context.Configuration.LazyLoadingEnabled = false;
+
+            Product product = context.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(product);
+        }
+
+        // PUT: api/Products/5
+        [ResponseType(typeof(void))]
+        public IHttpActionResult PutProduct(int id, Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            if (id != product.productID)
+            {
+                return BadRequest();
+            }
+
+            db.Entry(product).State = EntityState.Modified;
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!ProductExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return StatusCode(HttpStatusCode.NoContent);
+        }
+
+        // POST: api/Products
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult PostProduct(Product product)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            db.Products.Add(product);
+
+            try
+            {
+                db.SaveChanges();
+            }
+            catch (DbUpdateException)
+            {
+                if (ProductExists(product.productID))
+                {
+                    return Conflict();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return CreatedAtRoute("DefaultApi", new { id = product.productID }, product);
+        }
+
+        // DELETE: api/Products/5
+        [ResponseType(typeof(Product))]
+        public IHttpActionResult DeleteProduct(int id)
+        {
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return NotFound();
+            }
+
+            db.Products.Remove(product);
+            db.SaveChanges();
+
+            return Ok(product);
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private bool ProductExists(int id)
+        {
+            return db.Products.Count(e => e.productID == id) > 0;
+        }
+    }
+}
---
+++ WebApplication6/WebApplication6/scripts/app.js
@@ -0,0 +1,122 @@
+﻿// TODO: Replace with the URL of your WebService app
+var serviceUrl = 'http://localhost:59184/api/Products';
+
+function sendRequest() {
+    var method = $('#method').val();
+    var product = $('#product').val();
+    $.ajax({
+        type: method,
+        url: serviceUrl
+    }).done(function (data) {
+        data.forEach(function (val) {
+            callback(val)
+        });
+    }).error(function (jqXHR, textStatus, errorThrown) {
+        $('#value1').text(jqXHR.responseText || textStatus);
+    });
+}
+
+function callback(val) {
+    //  $("#products").replaceWith("<span id='value1'>(Result)</span>");
+    $("#value1").replaceWith("<ul id='products' />");
+    var str = "ProductID: " + val.productID + " Name: " + val.name + " mfg: " + val.mfg + " Vendor: " + val.vendor + " Price: " + val.price;
+    $('<li/>', { text: str }).appendTo($('#products'));
+}
+
+function find() {
+    var id = $('#productIdFind').val();
+    $.getJSON(serviceUrl + "/" + id,
+        function (data) {
+            if (data == null) {
+                $('#productFind').text('Product not found.');
+            }
+            var str = data.productID + ': ' + data.name + ':' + data.mfg + ':' + data.vendor + ':' + data.price + ':';
+            $('#productFind').text(str);
+        })
+    .fail(
+        function (jqueryHeaderRequest, textStatus, err) {
+            $('#productFind').text('Find error: ' + err);
+        });
+}
+
+// Add a new product.
+function create() {
+    jQuery.support.cors = true;
+    var product = {
+        productID: $('#txtAdd_productID').val(),
+        name: $('#txtAdd_name').val(),
+        mfg: $('#txtAdd_mfg').val(),
+        vendor: $('#txtAdd_vendor').val(),
+        price: $('#txtAdd_price').val()
+    };
+    var id = $('#productIdFind').val();
+
+    var cr = JSON.stringify(product);
+    $.ajax({
+        url: serviceUrl,
+        type: 'POST',
+        data: JSON.stringify(product),
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            $('#productCreate')
+                .text('Product successfully created.');
+            updateList();
+        },
+        error: function (_httpRequest, _status, _httpError) {
+            // XMLHttpRequest, textStatus, errorThrow
+            $('#productCreate')
+            .text('Error while adding product.  XMLHttpRequest:'
+                    + _httpRequest + '  Status: ' + _status
+                    + '  Http Error: ' + _httpError);
+        }
+    });
+}
+
+// Update a product object.
+function update() {
+    jQuery.support.cors = true;
+    var product = {
+        productID: $('#txtUpdate_productID').val(),
+        name: $('#txtUpdate_name').val(),
+        mfg: $('#txtUpdate_mfg').val(),
+        vendor: $('#txtUpdate_vendor').val(),
+        price: $('#txtUpdate_price').val()
+    };
+
+    var cr = JSON.stringify(product);
+    $.ajax({
+
+        url: serviceUrl + "/" + $('#txtUpdate_productID').val(),
+        type: 'PUT',
+        data: JSON.stringify(product),
+        contentType: "application/json;charset=utf-8",
+        success: function (data) {
+            $('#productUpdate')
+            .text('The update was successful.');
+            updateList();
+        },
+        error: function (_httpRequest, _status, _httpError) {
+            $('#productUpdate')
+            .text('Error while adding product.  XMLHttpRequest:'
+            + _httpRequest + '  Status: ' + _status + '  Http Error: '
+            + _httpError);
+        }
+    });
+}
+
+function del() {
+    var id = $('#productID').val();
+    $.ajax({
+        url: serviceUrl + "/" +id,
+        type: 'DELETE',
+        dataType: 'json',
+
+        success: function (data) {
+            $('#productDelete').text('Delete successful.');
+            updateList();
+        }
+    }).fail(
+        function (jqueryHeaderRequest, textStatus, err) {
+            $('#productDelete').text('Delete error: ' + err);
+        });
+}
